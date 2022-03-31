e913db5629753611b536dd63098cf81d000d559c
r_4spberry <ddtomilov@stud.etu.ru>
2022-03-31 23:45:54 +0300

    Naturals (almost) comleted
    Надо оптимизировать mod и добавить комментариев там, где их не хватает.

--- natnum.py
+++ natnum.py
@@ -1,21 +1,23 @@
+from logging import raiseExceptions
 from typing import List
 import copy
 
 class natural_number:
-    ''' 
+    """ 
     Натуральное число.
 
     Атрибуты класса:
         digits -- список цифр числа в обратном порядке
-    '''
+    """
     
     def __init__(self, x = 0):
-        '''
+        """
         Конструктор числа.
         Принимает на вход x: целое положительное число в формате int,
         либо строку цифр, его представляющее, преобразовывая в массив цифр digits.
+        
         Выполнил: Томилов Даниил
-        '''
+        """
         
         dig = "0123456789"
         
@@ -40,11 +42,12 @@ class natural_number:
                 raise ValueError(f"invalid digit for natural number: '{i}'")
 
     def is_zero(self) -> bool:
-        '''
+        """
         NZER_N_B
         Проверяет, является ли число нулём.
+        
         Выполнил: Томилов Даниил
-        '''
+        """
         
         if self.digits == [0]:
             return True
@@ -52,11 +55,12 @@ class natural_number:
             return False
     
     def add_1(self):
-        '''
+        """
         ADD_1N
         Добавляет единицу к числу
+        
         Выполнил: Томилов Даниил
-        '''
+        """
         
         self.digits.append(0)
         self.digits[0] += 1
@@ -68,18 +72,20 @@ class natural_number:
             self.digits.pop()
 
     def __str__(self) -> str:
-        '''
+        """
         Возвращает str(self)
+        
         Выполнил: Томилов Даниил
-        '''
+        """
         s = ''.join(map(str, self.digits[::-1]))
         return s
     
     def __repr__(self) -> str:
-        '''
+        """
         Возвращает repr(self)
+        
         Выполнил: Томилов Даниил
-        '''
+        """
         
         return f"natural_number('{str(self)}')"
     
@@ -94,13 +100,14 @@ class natural_number:
     def remove_leading_zeros(self):
         while self.digits[-1] == 0 and len(self.digits) > 1:
             self.digits.pop()
-             
+    
 def nat_sum(n1, n2):
-    '''
+    """
     ADD_NN_N
     Складывает два натуральных числа
+    
     Выполнил: Томилов Даниил
-    '''
+    """
     
     n1 = copy.deepcopy(n1)
     n2 = copy.deepcopy(n2)
@@ -126,13 +133,14 @@ def nat_sum(n1, n2):
     #тут дальше весь нужный код со всеми нужными комментариями
 
 def nat_sub(n1, n2):
-    '''
+    """
     SUB_NN_N
     Вычитание из большего натурального меньшее
-    
     n1 > n2
     return n1 - n2
-    '''
+    
+    Выполнил: Томилов Даниил
+    """
     
     n1 = copy.deepcopy(n1)
     n2 = copy.deepcopy(n2)
@@ -155,15 +163,16 @@ def nat_sub(n1, n2):
     
 
 def nat_cmp(n1,n2):
-    '''
+    """
     COM_NN_D
     Сравнение двух чисел
     
     return 0: n1 == n2
     return 1: n1 < n2
     return 2: n1 > n2
+    
     Выполнил: Томилов Даниил
-    '''
+    """
     
     n1 = copy.deepcopy(n1)
     n2 = copy.deepcopy(n2)
@@ -185,15 +194,16 @@ def nat_cmp(n1,n2):
 
         return 2 if n1.digits[i] > n2.digits[i] else 1
 
-def mul_nat_by_digit(n, d):
-    '''
+def nat_mul_by_digit(n, d):
+    """
     MUL_ND_N
     Умножение натурального числа на однозначное (цифру)
     
     n: natural_number
     d: int, 0 <= d < 10
+    
     Выполнил: Томилов Даниил
-    '''
+    """
     
     n = copy.deepcopy(n)
     if type(n) is not natural_number:
@@ -215,15 +225,16 @@ def mul_nat_by_digit(n, d):
     
     return n
 
-def mul_nat_by_10_pow(n, k):
-    '''
+def nat_mul_by_10_pow(n, k):
+    """
     MUL_Nk_N
     Умножение натурального числа на 10^k
     
     n: natural_number
     k: integer or natual_number
+    
     Выполнил: Томилов Даниил
-    '''
+    """
     
     if type(n) is not natural_number:
         raise TypeError('n must be a natural number')
@@ -244,7 +255,7 @@ def mul_nat_by_10_pow(n, k):
     
     return n
 
-def mul_nat(n1, n2):
+def nat_mul(n1, n2):
     """
     Умножение натурального n1 на n2
 
@@ -262,8 +273,197 @@ def mul_nat(n1, n2):
     ans = natural_number()
     
     for i in range(len(n2.digits)):
-        t = mul_nat_by_digit(n1, n2.digits[i])
-        t = mul_nat_by_10_pow(t, i)
+        t = nat_mul_by_digit(n1, n2.digits[i])
+        t = nat_mul_by_10_pow(t, i)
         ans = nat_sum(ans, t)
         
     return ans
+
+def SUB_NDN_N(n1, n2, d):
+    """
+    Возвращает результат выражения n1-n2*d > 0
+    n1: natural number
+    n2: natural number
+    d: integer, 0 <= d < 10
+    
+    Выполнил: Томилов Даниил
+    """
+    
+    if type(n1) is not natural_number:
+        raise TypeError("n1 must be a natural number")
+    if type(n2) is not natural_number:
+        raise TypeError('n2 must be a natural number')
+    if type(d) is not int:
+        raise TypeError('d must be an integer')
+    
+    if d < 0 or d >= 10:
+        raise ValueError("d must be a digit")
+    
+    n1 = copy.deepcopy(n1)
+    n2 = copy.deepcopy(n2)
+    
+    n2 = nat_mul_by_digit(n2, d)
+    
+    if nat_cmp(n1, n2) == 1:
+        raise ValueError("result must be not-negative")
+    
+    n1 = nat_sub(n1, n2)
+    
+    return n1
+
+def DIV_NN_Dk(n1,n2):
+    """
+    Вычисление первой цифры деления n1 на n2,
+    домноженное на 10^k,где k - номер позиции этой цифры
+    n1 >= n2
+    
+    Выполнил: Томилов Даниил
+    """
+    
+    if type(n1) is not natural_number:
+        raise TypeError("n1 must be a natural number")
+    if type(n2) is not natural_number:
+        raise TypeError("n2 must be a natural number")
+    
+    n1 = copy.deepcopy(n1)
+    n2 = copy.deepcopy(n2)
+    
+    if nat_cmp(n1, n2) == 1:
+        raise ValueError("n1 must be greater than or equal to n2")
+    
+    if nat_cmp(n1, natural_number(0)) == 0:
+        raise ZeroDivisionError
+    
+    t = natural_number(n1.digits.pop())
+    while nat_cmp(t, n2) == 1:
+        t.digits.insert(0, (n1.digits.pop()))
+    
+    ans = natural_number()
+    
+    while True:
+        try:
+            t = nat_sub(t, n2)
+            ans.add_1()
+            
+        except ValueError:
+            break
+    ans = nat_mul_by_10_pow(ans, len(n1.digits))
+    
+    return ans
+
+def nat_div(n1, n2):
+    """
+    DIV_NN_N
+    Частное от деления n1 на n2 с остатком
+    n1 >= n2
+    
+    Выполнил: Томилов Даниил
+    """
+    
+    if type(n1) is not natural_number:
+        raise TypeError("n1 must be a natural number")
+    if type(n2) is not natural_number:
+        raise TypeError("n2 must be a natural number")
+    
+    n1 = copy.deepcopy(n1)
+    n2 = copy.deepcopy(n2)
+    
+    if nat_cmp(n1, n2) == 1:
+        raise ValueError("n1 must be greater than or equal to n2")
+    
+    ans = natural_number()
+    while nat_cmp(n1, n2) != 1:
+        t = DIV_NN_Dk(n1, n2)
+        ans = nat_sum(ans, t)
+        n1 = nat_sub(n1, nat_mul(n2, t))
+    return ans
+
+def nat_mod(n1, n2):
+    """
+    DIV_NN_N
+    Остаток от деления n1 на n2 с остатком
+    n1 >= n2
+    
+    Выполнил: Томилов Даниил
+    """
+    
+    n1 = copy.deepcopy(n1)
+    n2 = copy.deepcopy(n2)
+    
+    ans = nat_div(n1, n2)
+    ans = nat_sub(n1, nat_mul(ans, n2))
+    
+    return ans
+
+def nat_gcd(n1, n2):
+    """
+    GCF_NN_N
+    НОД натуральных чисел n1 и n2
+    Как минимум n1 или n2 должно не равняться нулю
+    
+    Выполнил: Томилов Даниил
+    """
+    
+    n1 = copy.deepcopy(n1)
+    n2 = copy.deepcopy(n2)
+    
+    if nat_cmp(n1, n2) == 1:
+        t = copy.deepcopy(n1)
+        n1 = copy.deepcopy(n2)
+        n2 = t
+        
+    if nat_cmp(n1, natural_number(0)) == 0:
+        raise ValueError("at least one of n1 and n2 must be non-zero")
+    
+    if nat_cmp(n2, natural_number(0)) == 0:
+        return natural_number(0)
+    
+    while True:
+        c = nat_mod(n1, n2)
+        if nat_cmp(c, natural_number(0)) == 0:
+            return n2
+        n1 = copy.deepcopy(n2)
+        n2 = copy.deepcopy(c)
+    
+def nat_lcm(n1, n2):
+    """
+    LCM_NN_N
+    НОК натуральных чисел n1 и n2
+    n1 и n2 должны быть больше нуля
+    
+    Выполнил: Томилов Даниил
+    """
+    
+    n1 = copy.deepcopy(n1)
+    n2 = copy.deepcopy(n2)
+
+    m = nat_mul(n1, n2)
+    gcc = nat_gcd(n1, n2)
+    if nat_cmp(gcc, natural_number(0)) == 0:
+        raise ValueError("n1 and n2 must be positive")
+    return nat_div(m, gcc)
+
+# def main():
+#     from math import gcd, lcm
+#     from random import randrange as rr
+    
+#     for i in range(2, 100):
+#         print(i)
+#         a = rr(1, i)
+#         b = rr(1, i)
+#         n1 = natural_number(a)
+#         n2 = natural_number(b)
+#         ngcc = nat_gcd(n1, n2)
+#         nlcm = nat_lcm(n1, n2)
+#         if ngcc != gcd(a,b) or nlcm != lcm(a,b):
+#             print(a, b, ngcc, gcd(a,b), nlcm, lcm(a,b))
+
+def main():
+    a, b = input().split()
+    n1 = natural_number(a)
+    n2 = natural_number(b)
+    print(f"НОД: {nat_gcd(n1,n2)}, НОК: {nat_lcm(n1,n2)}")
+
+if __name__ == '__main__':
+    main()
+    
--- tests/test_natnum.py
+++ tests/test_natnum.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import random
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
@@ -82,48 +82,125 @@ class TestNat(unittest.TestCase):
         self.assertEqual(n2, natural_number(0))
     
     def test_mul_by_digit(self):
-        self.assertEqual(mul_nat_by_digit(natural_number(10), 0),
+        self.assertEqual(nat_mul_by_digit(natural_number(10), 0),
                          natural_number(0))
-        self.assertEqual(mul_nat_by_digit(natural_number(2), 2),
+        self.assertEqual(nat_mul_by_digit(natural_number(2), 2),
                          natural_number(4))
-        self.assertEqual(mul_nat_by_digit(natural_number(8), 2),
+        self.assertEqual(nat_mul_by_digit(natural_number(8), 2),
                          natural_number(16))
-        self.assertEqual(mul_nat_by_digit(natural_number(7), 1),
+        self.assertEqual(nat_mul_by_digit(natural_number(7), 1),
                          natural_number(7))
-        self.assertEqual(mul_nat_by_digit(natural_number(0), 9),
+        self.assertEqual(nat_mul_by_digit(natural_number(0), 9),
                          natural_number(0))
         
-        self.assertRaises(TypeError, mul_nat_by_digit, natural_number(1), "s")
-        self.assertRaises(TypeError, mul_nat_by_digit, "s", 3)
-        self.assertRaises(ValueError, mul_nat_by_digit, natural_number(1), 15)
+        self.assertRaises(TypeError, nat_mul_by_digit, natural_number(1), "s")
+        self.assertRaises(TypeError, nat_mul_by_digit, "s", 3)
+        self.assertRaises(ValueError, nat_mul_by_digit, natural_number(1), 15)
+        self.assertRaises(ValueError, nat_mul_by_digit, natural_number(1), -5)
         
     def test_mul_by_10_pow(self):
-        self.assertEqual(mul_nat_by_10_pow(natural_number(3), 3), 
+        self.assertEqual(nat_mul_by_10_pow(natural_number(3), 3), 
                          natural_number(3000))
-        self.assertEqual(mul_nat_by_10_pow(natural_number(3), natural_number(3)), 
+        self.assertEqual(nat_mul_by_10_pow(natural_number(3), natural_number(3)), 
                          natural_number(3000))
-        self.assertEqual(mul_nat_by_10_pow(natural_number(0), 3), 
+        self.assertEqual(nat_mul_by_10_pow(natural_number(0), 3), 
                          natural_number(0))
-        self.assertEqual(mul_nat_by_10_pow(natural_number(44), 3), 
+        self.assertEqual(nat_mul_by_10_pow(natural_number(44), 3), 
                          natural_number(44000))
         
         
-        self.assertRaises(TypeError, mul_nat_by_10_pow, natural_number(1), "s")
-        self.assertRaises(TypeError, mul_nat_by_10_pow, "s", natural_number(1))
+        self.assertRaises(TypeError, nat_mul_by_10_pow, natural_number(1), "s")
+        self.assertRaises(TypeError, nat_mul_by_10_pow, "s", natural_number(1))
     
     def test_mul(self):
-        self.assertRaises(TypeError, mul_nat, natural_number(1), "s")
-        self.assertRaises(TypeError, mul_nat, "s", natural_number(1))
+        self.assertRaises(TypeError, nat_mul, natural_number(1), "s")
+        self.assertRaises(TypeError, nat_mul, "s", natural_number(1))
         
-        self.assertEqual(mul_nat(natural_number(12345679), natural_number(27)), 
+        self.assertEqual(nat_mul(natural_number(12345679), natural_number(27)), 
                          natural_number(333333333))
-        self.assertEqual(mul_nat(natural_number(12345679), natural_number(0)), 
+        self.assertEqual(nat_mul(natural_number(12345679), natural_number(0)), 
                          natural_number(0))
-        self.assertEqual(mul_nat(natural_number(0), natural_number(27)), 
+        self.assertEqual(nat_mul(natural_number(0), natural_number(27)), 
                          natural_number(0))
-        self.assertEqual(mul_nat(natural_number(12345679), natural_number(1)), 
+        self.assertEqual(nat_mul(natural_number(12345679), natural_number(1)), 
                          natural_number(12345679))
         
+    def test_sub_mul(self):
+        self.assertRaises(TypeError, SUB_NDN_N, natural_number(1), "s", 1)
+        self.assertRaises(TypeError, SUB_NDN_N, "s", natural_number(1), 1)
+        self.assertRaises(ValueError, SUB_NDN_N, natural_number(1), natural_number(1), 15)
+        self.assertRaises(ValueError, SUB_NDN_N, natural_number(1), natural_number(1), -15)
+        self.assertRaises(ValueError, SUB_NDN_N, natural_number(1), natural_number(1), 5)
+        
+        self.assertEqual(SUB_NDN_N(natural_number(100), natural_number(5), 6), natural_number(70))
+        self.assertEqual(SUB_NDN_N(natural_number(101), natural_number(5), 1), natural_number(96))
+        self.assertEqual(SUB_NDN_N(natural_number(100), natural_number(5), 0), natural_number(100))
+        self.assertEqual(SUB_NDN_N(natural_number(100), natural_number(0), 6), natural_number(100))
+        self.assertEqual(SUB_NDN_N(natural_number(0), natural_number(5), 0), natural_number(0))
+        self.assertEqual(SUB_NDN_N(natural_number(0), natural_number(0), 0), natural_number(0))
+    
+    def test_DIV_NN_Dk(self):
+        self.assertRaises(TypeError, DIV_NN_Dk, natural_number(1), "s")
+        self.assertRaises(TypeError, DIV_NN_Dk, "s", natural_number(1))
+        self.assertRaises(ValueError, DIV_NN_Dk, natural_number(2), natural_number(10))
+        
+        self.assertEqual(DIV_NN_Dk(natural_number(123456), natural_number(432)), 200)
+        self.assertEqual(DIV_NN_Dk(natural_number(123456), natural_number(123456)), 1)
+        self.assertEqual(DIV_NN_Dk(natural_number(123456), natural_number(123455)), 1)
+        self.assertEqual(DIV_NN_Dk(natural_number(10), natural_number(2)), 5)
+        
+        n1 = natural_number(random.randrange(434, 35234))
+        d = 5435
+        n2 = nat_mul(n1, natural_number(d))
+        self.assertEqual(DIV_NN_Dk(n2, n1), 5000)
+    
+    def test_nat_div(self):
+        self.assertRaises(TypeError, nat_div, natural_number(1), "s")
+        self.assertRaises(TypeError, nat_div, "s", natural_number(1))
+        self.assertRaises(ValueError, nat_div, natural_number(2), natural_number(10))
+        
+        self.assertEqual(nat_div(natural_number(123456), natural_number(432)), 285)
+        self.assertEqual(nat_div(natural_number(123456), natural_number(123456)), 1)
+        self.assertEqual(nat_div(natural_number(123456), natural_number(123455)), 1)
+        self.assertEqual(nat_div(natural_number(10), natural_number(2)), 5)
+        self.assertEqual(nat_div(natural_number(13), natural_number(2)), 6)
+        n1 = natural_number(random.randrange(434, 35234))
+        d = 5435
+        n2 = nat_mul(n1, natural_number(d))
+        n2 = nat_sum(n2, natural_number(15))
+        self.assertEqual(nat_div(n2, n1), d)
+        self.assertEqual(nat_div(n1, natural_number(1)), n1)
+
+    def test_nat_mod(self):
+        self.assertRaises(TypeError, nat_mod, natural_number(1), "s")
+        self.assertRaises(TypeError, nat_mod, "s", natural_number(1))
+        self.assertRaises(ValueError, nat_mod, natural_number(2), natural_number(10))
+        
+        self.assertEqual(nat_mod(natural_number(123456), natural_number(432)), 336)
+        self.assertEqual(nat_mod(natural_number(123456), natural_number(1)), 0)
+        self.assertEqual(nat_mod(natural_number(123456), natural_number(123456)), 0)
+        self.assertEqual(nat_mod(natural_number(123456), natural_number(123455)), 1)
+        self.assertEqual(nat_mod(natural_number(10), natural_number(2)), 0)
+        self.assertEqual(nat_mod(natural_number(13), natural_number(2)), 1)
+        n1 = natural_number(random.randrange(434, 35234))
+        d = 5435
+        n2 = nat_mul(n1, natural_number(d))
+        n2 = nat_sum(n2, natural_number(15))
+        self.assertEqual(nat_mod(n2, n1), 15)
+        
+    def test_gcd(self):
+        self.assertEqual(nat_gcd(natural_number(1), natural_number(1)), 1)
+        self.assertEqual(nat_gcd(natural_number(33), natural_number(33)), 33)
+        self.assertEqual(nat_gcd(natural_number(124), natural_number(16)), 4)
+        self.assertEqual(nat_gcd(natural_number(15), natural_number(0)), 0)
+        self.assertEqual(nat_gcd(natural_number(363), natural_number(3)), 3)
+
+    def test_lcm(self):
+        self.assertEqual(nat_lcm(natural_number(1), natural_number(1)), 1)
+        self.assertEqual(nat_lcm(natural_number(124), natural_number(16)), 496)
+        self.assertRaises(ValueError, nat_lcm, natural_number(15), natural_number(0))
+        self.assertEqual(nat_lcm(natural_number(363), natural_number(3)), 363)
+    
 def main():
     unittest.main()
     
